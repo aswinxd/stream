e4deb92b385399ee89b16e130497887197a8afec
aswinxd <155294690+aswinxd@users.noreply.github.com>
2024-06-12 14:39:06 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+
+API_ID = "12799559"
+API_HASH = "077254e69d93d08357f25bb5f4504580"
+BOT_TOKEN = "1810353153:AAFgQfh_bs0mO1nGRCmCexywpIbC2BqDe_o"
+DURATION_LIMIT = 600  # in seconds, for example, 10 minutes
